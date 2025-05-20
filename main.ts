cb382d13ca3f252f25ce7af29ea821435c98a8eb
FANExWOLF <ahammes@tnsoci.org>
2025-05-20 13:56:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showString("Mary had a little lamb")
+music.play(music.stringPlayable("A G F G A A A - ", 120), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("G G G - A C5 C5 - ", 120), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("A G F G A A A - ", 120), music.PlaybackMode.UntilDone)
+music.play(music.stringPlayable("G G A G F - - - ", 120), music.PlaybackMode.UntilDone)
+basic.forever(function () {
+	
+})
